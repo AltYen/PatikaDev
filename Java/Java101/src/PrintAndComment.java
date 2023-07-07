b7c77af4e1ed0e033b90debf5f7dd9261b74833c
AltYen <altanyenigun@gmail.com>
2023-07-07 18:19:46 +0300

    very very basic of java

---
+++ Java/Java101/src/PrintAndComment.java
@@ -0,0 +1,23 @@
+/**
+ * @author Altan Yenigun, 07.07.2023
+ */
+public class PrintAndComment {
+    public static void main(String[] args) {
+        /*
+        System.out.println("Java101 Patikası");
+        System.out.print("Başlıyoruz");
+        System.out.print("Java\r101\b \t Patikası\nBaşlıyoruz\n");
+        System.out.print("Java101\'de\n");
+        System.out.print("Java101 \"Dersleri\"\n");
+        System.out.print("Java101\\src\\test ");
+        */
+
+        /*  birden fazla yorum satırı yöntemi
+         * 1.satır
+         * 2.satır
+         */
+
+        //burası programın başlangıç noktası.
+        System.out.println("Burası Programın başlangıcı");
+    }
+}
