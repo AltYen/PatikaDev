d8ae70f5f2d245381b29437cc90468e2eb75a9c9
AltYen <altanyenigun@gmail.com>
2023-07-26 14:01:23 +0300

    object oriented student note example

---
+++ Java/Java101/src/BasicOfJava/Classes/OgrenciNotSistemi/Course.java
@@ -0,0 +1,32 @@
+package BasicOfJava.Classes.OgrenciNotSistemi;
+
+public class Course {
+    //bir sınıfın niteliğine diğer bir sınıfı nitelik olarak atayabiliriz.
+    Teacher teacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+
+    Course(String name,String code,String prefix){
+        this.name=name;
+        this.code=code;
+        this.prefix=prefix;
+        int note=0;
+    }
+
+    void addTeacher(Teacher teacher){
+        if(teacher.branch.equals(this.prefix)){
+            this.teacher=teacher;
+        }
+        else
+            System.out.println("Öğretmen ve Ders Bölümleri uyuşmuyor !");
+    }
+
+    void printTeacherInfo(){
+        // Teacher sınıfından üretilmiş nesne'ye ait methodun çağrılması.
+        this.teacher.print();
+    }
+
+
+}
---
+++ Java/Java101/src/BasicOfJava/Classes/OgrenciNotSistemi/Main.java
@@ -0,0 +1,28 @@
+package BasicOfJava.Classes.OgrenciNotSistemi;
+
+public class Main {
+    public static void main(String[] args) {
+        Teacher t1= new Teacher("Mahmut","TRH","905426528979");
+        Teacher t2= new Teacher("Graham Bell","FZK","905426528989");
+        Teacher t3= new Teacher("Külyatmaz","BIO","905426528990");
+
+        Course tarih = new Course("Tarih","101","TRH");
+        tarih.addTeacher(t1);//Teacher sınıfından üretilmiş nesneyi Course sınıfındaki Teacher niteliğine atıyoruz.
+
+        Course fizik = new Course("Fizik","102","FZK");
+        fizik.addTeacher(t2);
+
+        Course biyo = new Course("Biyoloji","101","BIO");
+        biyo.addTeacher(t3);
+
+
+        Student s1 = new Student("Inek Saban","123","4",tarih,fizik,biyo);
+        s1.addBulkExamNote(100,78,50);
+        s1.isPass();
+
+        Student s2 = new Student("Guduk Necmi","444","4",tarih,fizik,biyo);
+        s2.addBulkExamNote(50,30,70);
+        s2.isPass();
+
+    }
+}
