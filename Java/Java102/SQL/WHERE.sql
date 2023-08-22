274b6c49c2e358acf35fea60091ce43fed5b77d9
AltYen <altanyenigun@gmail.com>
2023-08-22 11:32:09 +0300

    sql where example

---
+++ Java/Java102/SQL/WHERE.sql
@@ -0,0 +1,20 @@
+/*
+SELECT * FROM film 
+WHERE rental_rate = 0.99;
+
+SELECT * FROM actor
+WHERE first_name = 'Penelope';
+
+SELECT * FROM film
+WHERE length<=90;
+
+SELECT * FROM film
+WHERE rental_rate<=1;
+
+SELECT * FROM film
+WHERE rental_rate!=0.99;
+*/
+
+SELECT * FROM film
+WHERE rental_rate <> 4.99;
+
