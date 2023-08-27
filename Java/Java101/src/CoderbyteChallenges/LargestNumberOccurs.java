db4ff6dad7c9f8df10150c427ce5252ec9b8a908
AltYen <altanyenigun@gmail.com>
2023-08-27 19:38:02 +0300

    refactor - find largest element 'k' from the array that is repeated exactly 'k' times

--- Java/Java101/src/CoderbyteChallenges/LargestNumberOccurs.java
+++ Java/Java101/src/CoderbyteChallenges/LargestNumberOccurs.java
@@ -5,26 +5,27 @@ public class LargestNumberOccurs {
         int maxNumber = 0;
 
         for(int i = 0;i<arr.length;i++){
-            if(arr[i]>maxNumber){
-                if(getRepeatCount(arr,arr[i]) == arr[i]){
+            if(arr[i]>maxNumber && checkOccursCount(arr,arr[i])){
                     maxNumber=arr[i];
-                }
             }
         }
         System.out.println(maxNumber);
     }
 
-    static int getRepeatCount(int[] arr, int number){
+    static boolean checkOccursCount(int[] arr, int number){
         int count = 0;
         for(int i=0;i<arr.length;i++){
             if(number == arr[i])
                 count++;
         }
-        return count;
+        if(count == number)
+            return true;
+        else
+            return false;
     }
     public static void main(String[] args) {
         //Given an array of integers and an integer ‘k’, the task is to find the largest element from the array that is repeated exactly ‘k’ times.
-        int[] arr = {3,8,2,3,3,2};
+        int[] arr = {2,5,6,3,11,9,1};
         calculate(arr);
     }
 }
