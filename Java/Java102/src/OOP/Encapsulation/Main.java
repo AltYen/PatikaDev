c529befacbc1a134909737fd1a097cc6ca5f2cbb
AltYen <altanyenigun@gmail.com>
2023-08-29 12:35:59 +0300

    basic of encapsulation

---
+++ Java/Java102/src/OOP/Encapsulation/Main.java
@@ -0,0 +1,12 @@
+package OOP.Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Book harryPotter = new Book("Harry Potter",150);
+        Book lordOfTheRings = new Book("Lord Of The Rings",-100);
+
+        lordOfTheRings.setPageNumber(-1000);
+        System.out.println(lordOfTheRings.getPageNumber());
+        System.out.println(lordOfTheRings.getName());
+    }
+}
