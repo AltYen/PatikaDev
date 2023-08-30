a7ff648e50965c0e4db7a4b929c61e8cab3fed74
AltYen <altanyenigun@gmail.com>
2023-08-30 13:36:18 +0300

    AdventureGame | Character selection

---
+++ Java/Java102/src/OOP/AdventureGame/Game.java
@@ -0,0 +1,16 @@
+package OOP.AdventureGame;
+
+import java.util.Scanner;
+
+public class Game {
+    private Scanner input = new Scanner(System.in);
+    public void start(){
+        System.out.println("Macera Oyununa Hoşgeldiniz !");
+        System.out.println("Lütfen bir isim giriniz : ");
+        //String playerName = input.nextLine();
+        Player player = new Player("Mustafa");
+        System.out.println("Sayın " + player.getName() + " bu karanlık ve sisli adaya Hoşgeldiniz !! Burada yaşananların hepsi gerçek!");
+        System.out.println("Lütfen bir karakter seçiniz ");
+        player.selectChar();
+    }
+}
